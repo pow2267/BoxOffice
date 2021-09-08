689c95dac49892980058eb84437086dca566594b
pow2267 <pow2267@gmail.com>
2021-09-08 14:41:51 +0900

    Set movie poster And Allow http transport

--- BoxOffice/TableViewController.swift
+++ BoxOffice/TableViewController.swift
@@ -40,6 +40,26 @@ class TableViewController: UIViewController, UITableViewDataSource {
             cell.ratesImage.image = UIImage(named: "ic_allages")
         }
         
+        // nil처리를 해줘야 cell 재활용 할 때 다른 이미지가 잘못 들어가지 않음
+        cell.posterView.image = nil
+        
+        DispatchQueue.global().async {
+            // Data는 동기 메소드라서 이미지를 불러올 때까지 동작이 멈추게 됨. 그럼 불편하니까 백그라운드 큐에 넣어줌
+            guard let imageUrl: URL = URL(string: movie.thumb), let imageData: Data = try? Data(contentsOf: imageUrl) else {
+                return
+            }
+            
+            DispatchQueue.main.async {
+                // image를 셋팅하기 전에 사용자가 스크롤을 하면 화면에 보여지는 cell의 index가 달라질 수 있으므로 index 비교 후 이미지 삽입
+                if let index: IndexPath = tableView.indexPath(for: cell) {
+                    if index.row == indexPath.row {
+                        cell.posterView.image = UIImage(data: imageData)
+                        cell.setNeedsLayout()
+                    }
+                }
+            }
+        }
+        
         return cell
     }
     
@@ -64,7 +84,7 @@ class TableViewController: UIViewController, UITableViewDataSource {
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
         
-        // Todo
+        // Todo: 정렬 순서 동적으로 적용 필요
         requestMovies(0)
     }
 }
