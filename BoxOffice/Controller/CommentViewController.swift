a4b261006b79eeed0fe32a8c3cd4ada648fdc698
pow2267 <pow2267@gmail.com>
2021-09-25 23:30:10 +0900

    Change star images to button And Add function that counts rate

--- BoxOffice/Controller/CommentViewController.swift
+++ BoxOffice/Controller/CommentViewController.swift
@@ -16,11 +16,16 @@ class CommentViewController: UIViewController {
     @IBOutlet weak var nicknameField: UITextField!
     @IBOutlet weak var commentField: UITextField!
     @IBOutlet weak var rateLabel: UILabel!
+    @IBOutlet weak var ratingStars: StarsStackView!
     
     var movie: Movie?
         
     @IBAction func touchUpCancelButton() {
         self.dismiss(animated: true, completion: nil)
+    }
+    
+    @objc func changeRate(_ sender: UIButton) {
+        self.rateLabel.text = String(sender.tag)
     }
 
     override func viewDidLoad() {
@@ -44,6 +49,14 @@ class CommentViewController: UIViewController {
         
         if let nickname = UserInfo.shared.nickname {
             self.nicknameField.text = nickname
+        }
+        
+        guard let stars: [UIButton] = self.ratingStars.subviews.filter({ $0 is UIButton }) as? [UIButton] else {
+            return
+        }
+        
+        for star in stars {
+            star.addTarget(self, action: #selector(changeRate(_:)), for: .touchUpInside)
         }
     }
 }
